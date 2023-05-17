9fcd49e1b5fbc6b664e77972652650fb4c1c6686
tsaihsiaoyi <tsaihsiaoyi0228@gmail.com>
2023-05-17 16:22:41 +0800

    update

--- PYXAID/src/pyxaid.cpp
+++ PYXAID/src/pyxaid.cpp
@@ -11,64 +11,43 @@
 #include "namd.h"
 using namespace std;
 
-class info
+void version()
 {
-
-public:
-  // Constructor
-  info() {}
-
-  // Class members
-  void version()
-  {
-    cout << "================================================================================\n";
-    cout << "PYXAID: PYthon eXtension for Ab Inition Dynamics version 1.0\n";
-    cout << "/***********************************************************\n";
-    cout << " * Copyright (C) 2013 PYXAID group\n";
-    cout << " * This program is free software distributed under the terms of the\n";
-    cout << " * GNU General Public License as published by the\n";
-    cout << " * Free Software Foundation; either version 3 of the\n";
-    cout << " * License, or (at your option) any later version.\n";
-    cout << " * http://www.gnu.org/copyleft/gpl.txt\n";
-    cout << "***********************************************************/\n";
-  }
-
-  void developers()
-  {
-    cout << "===== Name ========================== Contact info ==============\n";
-    cout << "Alexey V. Akimov              e-mail: alexvakimov@gmail.com      \n";
-    cout << "Oleg V. Prezhdo               e-mail: oprezhdo@chem.rochester.edu\n";
-    cout << "...\n";
-  }
-
-  void documentation()
-  {
-    cout << "Coming soon...\n";
-  }
-};
-
-// BOOST_PYTHON_MODULE(pyxaid_core)
-// {
-
-//   class_<info>("info", init<>())
-//       .def("version", &info::version)
-//       .def("developers", &info::developers)
-//       .def("documentation", &info::documentation);
-
-//   export_namd();
-// }
-
-int main()
-{
-  cout << "============================================================\n";
+  cout << "================================= Information ==================================\n";
   cout << "PYXAID: PYthon eXtension for Ab Inition Dynamics version 1.0\n";
   cout << "/***********************************************************\n";
+  cout << " * Copyright (C) 2013 PYXAID group\n";
   cout << " * This program is free software distributed under the terms of the\n";
   cout << " * GNU General Public License as published by the\n";
   cout << " * Free Software Foundation; either version 3 of the\n";
   cout << " * License, or (at your option) any later version.\n";
   cout << " * http://www.gnu.org/copyleft/gpl.txt\n";
   cout << "***********************************************************/\n";
+  cout << "================================================================================\n";
+}
+
+void developers()
+{
+  cout << "===== Name ===================== Developers ======== Contact info ==============\n";
+  cout << "Alexey V. Akimov                             e-mail: alexvakimov@gmail.com      \n";
+  cout << "Oleg V. Prezhdo                              e-mail: oprezhdo@chem.rochester.edu\n";
+  cout << "Hsiaoyi Tsai                                 e-mail: tsaihsiaoyi0228@gmail.com  \n";
+  cout << "...\n";
+  cout << "================================================================================\n";
+}
+
+void documentation()
+{
+  cout << "================================ Documentation =================================\n";
+  cout << "Coming soon...\n";
+  cout << "================================================================================\n";
+}
+
+int main()
+{
+  version();
+  developers();
+  documentation();
   namd();
   return 0;
 }
